f21de7f5a412b0a9d1a71b7e67cc234326801526
Rahul <raulrag009@gmail.com>
2016-01-17 23:30:56 +0530

    Changed server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
-	res.send('hello world web');
+	res.send('Comon keep running asdadfs');
 });
 app.listen(port, ipaddress);
