e1bce767059dcab2a9d074da6079bc49c771f36e
rahulk2014 <rahul014@ccs.neu.edu>
2016-03-02 23:12:09 -0500

    Implemented register functionality

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -11,61 +11,83 @@
 
     function UserServiceFunction($rootScope) {
 
-        var users = [
-            {
-                "_id": 123, "firstName": "Alice", "lastName": "Wonderland",
-                "username": "alice", "password": "alice", "roles": ["student"]
-            },
-            {
-                "_id": 234, "firstName": "Bob", "lastName": "Hope",
-                "username": "bob", "password": "bob", "roles": ["admin"]
-            },
-            {
-                "_id": 345, "firstName": "Charlie", "lastName": "Brown",
-                "username": "charlie", "password": "charlie", "roles": ["faculty"]
-            },
-            {
-                "_id": 456, "firstName": "Dan", "lastName": "Craig",
-                "username": "dan", "password": "dan", "roles": ["faculty", "admin"]
-            },
-            {
-                "_id": 567, "firstName": "Edward", "lastName": "Norton",
-                "username": "ed", "password": "ed", "roles": ["student"]
+        var model = {
+                users : [
+                {
+                    "_id": 123, "firstName": "Alice", "lastName": "Wonderland",
+                    "username": "alice", "password": "alice", "roles": ["student"]
+                },
+                {
+                    "_id": 234, "firstName": "Bob", "lastName": "Hope",
+                    "username": "bob", "password": "bob", "roles": ["admin"]
+                },
+                {
+                    "_id": 345, "firstName": "Charlie", "lastName": "Brown",
+                    "username": "charlie", "password": "charlie", "roles": ["faculty"]
+                },
+                {
+                    "_id": 456, "firstName": "Dan", "lastName": "Craig",
+                    "username": "dan", "password": "dan", "roles": ["faculty", "admin"]
+                },
+                {
+                    "_id": 567, "firstName": "Edward", "lastName": "Norton",
+                    "username": "ed", "password": "ed", "roles": ["student"]
+                }
+            ],
+
+            findUserByCredentials :findUserByCredentials,
+            findUserByUsername : findUserByUsername,
+            findAllUsers : findAllUsers,
+            createUser : createUser,
+            deleteUserById :deleteUserById,
+            updateUser : updateUser,
+            setCurrentUser : setCurrentUser,
+            getCurrentUser : getCurrentUser
+         };
+
+        return model;
+
+        function findUserByUsername (username) {
+            for (var u in model.users) {
+                if (model.users[u].username === username) {
+                    return model.users[u];
+                }
             }
-        ];
+            return null;
+        }
 
         function findUserByCredentials(username, password, callback) {
             for (var user in users) {
-                if (users[user].username === username && users[user].password === password)
-                    callback(users[user]);
+                if (model.users[user].username === username && model.users[user].password === password)
+                    callback(model.users[user]);
             }
             callback(null);
         }
 
         function findAllUsers(callback) {
-            callback(users);
+            callback(model.users);
         }
 
         function createUser(user, callback) {
             var newUser = {
                 username: user.username,
-                password : user.password
+                password : user.password,
+                email : user.email
              };
 
-            users.push(newUser);
+            model.users.push(newUser);
             callback(newUser);
         }
 
         function deleteUserById(userId, callback) {
-            var user_found = findUserById(userId);
-
+            var user_found = model.users[userId];
             if (null != user_found)
-                users.splice(users.indexOf(user), 1);
-            callback(users);
+                users.splice(model.users.indexOf(user), 1);
+            callback(model.users);
         }
 
         function updateUser(userId, user, callback) {
-            var user_found = findUserById(userId);
+            var user_found = model.users[userId];
             if (userId != null) {
                 user_found.username = user.username;
                 user_found.firstName = user.firstName;
--- public/assignment/views/users/register.controller.js
+++ public/assignment/views/users/register.controller.js
@@ -12,29 +12,36 @@
     function registerController($location, $scope, UserService) {
 
         $scope.error = null;
-        if (null == user) {
-            $scope.error = "Please fill in the required fields";
-            return;
+        $scope.register = function(user) {
+
+            if (null == user) {
+                $scope.error = "Please fill in the required fields";
+                return;
+            }
+            if (!user.username) {
+                $scope.error = "Please provide a username";
+                return;
+            }
+            if (!user.password || !user.confirmPassword) {
+                $scope.error = "Please provide a password";
+                return;
+            }
+            if (user.password != user.confirmPassword) {
+                $scope.error = "Passwords must match";
+                return;
+            }
+            var user = UserService.findUserByUsername(user.username);
+            if (user != null) {
+                $scope.error = "User already exists";
+                return;
+            }
+            UserService.createUser($scope.user, function (newUser) {
+                UserService.setCurrentUser(newUser);
+            });
+
+            //Navigate to the profile page
+            $location.url("/profile");
+
         }
-        if (!user.username) {
-            $scope.error = "Please provide a username";
-            return;
-        }
-        if (!user.password || !user.confirmPassword) {
-            $scope.error = "Please provide a password";
-            return;
-        }
-        if (user.password != user.confirmPassword) {
-            $scope.error = "Passwords must match";
-            return;
-        }
-        var user = UserService.findUserByUsername(user.username);
-        if (user != null) {
-            $scope.error = "User already exists";
-            return;
-        }
-        UserService.createUser($scope.user,function(newUser){
-            UserService.setCurrentUser(newUser);
-        });
     }
 })();
